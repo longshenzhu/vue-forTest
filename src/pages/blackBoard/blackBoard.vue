bfdc14baa66b498ffbde471e34e0e36ef153e920
longshenzhu <1223006618@qq.com>
2020-05-09 18:26:22 +0800

    save

---
+++ src/pages/blackBoard/blackBoard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="blackBoard">
+        <h2>这是黑板页</h2>
+        <my-canvas myCanvas="first"></my-canvas>
+        <my-canvas myCanvas="second"></my-canvas>
+    </div>
+</template>
+<script>
+import myCanvas from '@/components/myCanvas'
+export default {
+    name: "blackBoard",
+    data(){
+        return {
+            
+        }
+    },
+    components:{
+        "my-canvas" : myCanvas,     //myCanvas
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/ppt/pptPage.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <div class="pptPage">
+    <img id="ocrImgSign" :src="pageData.url" :alt="pageData.name" class="img" crossorigin="Anonymous" />
+  </div>
+</template>
+<script>
+    export default {
+        name: "pptPage",
+        props: {
+            index:"",
+            pageData:''
+        },
+        data(){
+            return {
+                
+            }
+        },
+        mounted(){
+            this.init();
+        },
+        methods:{
+            init(){
+                console.log(`这是第${this.pageData.index}页`)
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .pptPage {
+        width: 100%;
+        height: 100%;
+
+        .no-img {
+            margin-top: 1rem;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            h2 {
+            color: #ffffff;
+            }
+        }
+        .img {
+            display: block;
+            width: 100%;
+            height: 100%;
+        }
+    }
+</style>
