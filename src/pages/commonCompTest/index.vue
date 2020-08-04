90fd57eeb0181d47a5a68b83dac20911c7e28cbe
longshenzhu <1223006618@qq.com>
2020-08-04 13:42:14 +0800

    svae

--- src/pages/commonCompTest/index.vue
+++ src/pages/commonCompTest/index.vue
@@ -6,7 +6,7 @@
           <!-- 保存 -->
       </nb-button>
       <h3>具有数据校验功能的表单组件——Form</h3>
-      <i-form :model="formValidate" :rules="ruleValidate">
+      <i-form ref="form" :model="formValidate" :rules="ruleValidate">
         <i-form-item label="用户名" prop="name">
             <i-input v-model="formValidate.name"></i-input>
         </i-form-item>
@@ -14,7 +14,8 @@
             <i-input v-model="formValidate.mail"></i-input>
         </i-form-item>
       </i-form>
-
+       <button @click="handleSubmit">提交</button>
+       <button @click="handleReset">重置</button>
       <!-- <div>
           <label>i-input text</label>
           <i-form-item>
@@ -81,8 +82,25 @@ export default {
         inputEventHandler:function(eventValue){
             console.log("eventValue",eventValue);
         },
-        testClick: function(){
-            //console.log('blur:',this.testValue);
+        handleSubmit: function(){
+            // this.$refs.form.validate((valid) => {
+            //     if(valid){
+            //         window.alert('提交成功');
+            //     }else{
+            //         window.alert('表单校验失败');
+            //     }
+            // })
+            const validate = this.$refs.form.validate();
+            validate.then((valid) => {
+                if(valid){
+                    window.alert('提交成功');
+                }else{
+                    window.alert('表单校验失败');
+                }
+            })
+        },
+        handleReset(){
+            this.$refs.form.resetFields();
         }
     },
     created() {
